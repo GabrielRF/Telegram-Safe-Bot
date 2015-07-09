451112d6660fa60e89801ae6db8c55397062392a
Gabriel R F <github@gabrf.com>
2015-07-09 14:53:03 -0300

    Analytics
    Added analytics data gather to the bot.
    No logs are kept.

--- safe_bot.py
+++ safe_bot.py
@@ -7,9 +7,36 @@ import string
 import random
 import time
 
+from parse_rest.connection import register
+from parse_rest.datatypes import Object
+from parse_rest.user import User
+
 TOKEN = sys.argv[1]
+PARSE_APP_KEY = sys.argv[2]
+PARSE_REST_API_KEY = sys.argv[3]
+
 users = {}
 markuphide = types.ReplyKeyboardHide()
+
+class Usage(Object):
+    pass
+
+def update_analytics(m_chat_id, m_group):
+    set_group = 'user.' + str(m_group) + ' = 1'
+    obj_id = Usage.Query.filter(user_id=int(m_chat_id)).limit(1)
+    if len(str(obj_id)) < 4:
+        user = Usage()
+        user.user_id = int(m_chat_id)
+        exec(set_group)
+        user.save()
+    else:
+        try:
+            uid = str(obj_id)[8:-2]
+            user = Usage.Query.get(objectId=uid)
+            user.increment(str(m_group))
+            user.save()
+        except Exception:
+            pass
 
 class User:
     def __init__(self,chatid,text):
@@ -28,14 +55,17 @@ def listener(*messages):
         if users.has_key(chatid) and m.content_type == 'text':
             user = users.get(chatid)
             if text == '/start':
+                update_analytics(chatid, 'Start')
                 start = types.ReplyKeyboardMarkup()
                 start.row('Hash','Password')
                 tb.send_message(chatid,'Select one option',reply_markup=start)
             elif text == '/cancel':
+                update_analytics(chatid, 'Cancel')
                 cancel = types.ReplyKeyboardMarkup()
                 cancel.row('/start','/info')
                 tb.send_message(chatid,'Done',reply_markup=cancel)
             elif text == '/info':
+                update_analytics(chatid, 'Info')
                 info = ('This bot is under constant development!\n'
                 'If you have any question or suggestion,\n'
                 'please, talk to me!\nTwitter: grfgabriel\n'
@@ -45,6 +75,7 @@ def listener(*messages):
                 '\nRate the bot:\nhttps://telegram.me/storebot?start=safe_bot')
                 tb.send_message(chatid,info)
             elif text == 'Hash':
+                update_analytics(chatid, 'Hash')
                 hash = types.ReplyKeyboardMarkup()
                 hash.row('MD5','SHA1','SHA224')
                 hash.row('SHA256','SHA384','SHA512')
@@ -78,6 +109,7 @@ def listener(*messages):
             user.LastMessage(user,text)
         elif m.content_type != 'text': tb.send_message(chatid,'Please, send me only text.')
         else:
+            update_analytics(chatid, 'Text')
             user = User(chatid,text)
             users[chatid] = user
             Start(chatid,text)
@@ -104,6 +136,7 @@ def PassGen(size,chars):
     except:
         return 'I was expecting an integer.'
 
+register(PARSE_APP_KEY,PARSE_REST_API_KEY)
 tb = telebot.TeleBot(TOKEN)
 tb.set_update_listener(listener)
 tb.polling()
